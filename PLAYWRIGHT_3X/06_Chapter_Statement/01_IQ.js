47f5d82db92afb0073e46523c7a27fd218b9d116
patilchetana123 <chetupatel89@gmail.com>
2026-07-20 17:09:49 +0530

    Add chapter 6 statement files (01_IQ, 02_IQ2, 03_multiple_statement)
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ PLAYWRIGHT_3X/06_Chapter_Statement/01_IQ.js
@@ -0,0 +1,6 @@
+
+let a=27;
+
+if(a>25){
+    console.log("a is greater than 25");
+}   
---
+++ PLAYWRIGHT_3X/06_Chapter_Statement/02_IQ2.js
@@ -0,0 +1,11 @@
+
+let age = 26;
+if (age > 30) {
+    console.log("you are too old");
+} else if (age > 25) {
+    console.log("Drinking");
+} else if (age > 18) {
+    console.log("go to Goa");
+} else {
+    console.log("you can't drink");
+}
